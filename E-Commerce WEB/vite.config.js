158500b6008a4a57c2bd9c0b46c6c49f093e69e8
rohan sharma dutt <rohansharmadutt876@gmail.com>
2025-01-29 18:14:48 +0530

    new

--- E-Commerce WEB/vite.config.js
+++ E-Commerce WEB/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   build: {
     rollupOptions: {
-      external: ["react-router-dom"],
+      external: ["react-toastify"],
     },
   },
   plugins: [react()],
